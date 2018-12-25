b053538575fee36373d18c5a0c524bdb4c8de451
Dmitry Kartsev <dkartsev@redhat.com>
2018-12-25 14:10:22 +0200

    update print in consumer

--- src/main/java/com/redhat/kubakc/service/Consumer.java
+++ src/main/java/com/redhat/kubakc/service/Consumer.java
@@ -1,6 +1,9 @@
 package com.redhat.kubakc.service;
 
+import com.redhat.kubakc.controller.KStateImageController;
 import com.redhat.kubakp.model.Square;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -8,12 +11,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class Consumer {
 
+    private Logger logger = LoggerFactory.getLogger(KStateImageController.class);
+
     @Autowired
     KImageGenerator kImageGenerator;
 
     @KafkaListener(topics = "t10", groupId = "group1")
     public void consumeJson(Square square) {
-        System.out.println("Consumed Message: " + square.toString());
+        logger.info("Consumed Message: " + square.toString());
         kImageGenerator.setSquare(square);
         kImageGenerator.generateStateImage();
 
